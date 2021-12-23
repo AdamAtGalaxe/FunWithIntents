9c82c925d8c3c5d903cfb30e957acc5e9767db96
AdamAtGalaxe <95375208+AdamAtGalaxe@users.noreply.github.com>
2021-12-23 11:48:08 -0500

    flash done wrong

---
+++ app/src/main/java/com/galaxe/funwithintents/AnimationFrag1.kt
@@ -0,0 +1,22 @@
+package com.galaxe.funwithintents
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class AnimationFrag1 : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_animation_frag1, container, false)
+
+    }
+
+}
---
+++ app/src/main/java/com/galaxe/funwithintents/AnimationFrag2.kt
@@ -0,0 +1,22 @@
+package com.galaxe.funwithintents
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class AnimationFrag2 : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_animation_frag2, container, false)
+    }
+
+}
